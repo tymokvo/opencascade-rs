2dc36d39fe4e23c968239d1a00ac329566f9c582
Tyler Kvochick <tyler.kvochick@teecom.com>
2026-01-01 17:15:47 -0500

    Add sync impl for topo ds shape

--- crates/opencascade-sys/src/lib.rs
+++ crates/opencascade-sys/src/lib.rs
@@ -1514,6 +1514,7 @@ unsafe impl Send for ffi::TopoDS_Shell {}
 unsafe impl Send for ffi::TopoDS_Solid {}
 unsafe impl Send for ffi::TopoDS_Compound {}
 unsafe impl Send for ffi::TopoDS_Shape {}
+unsafe impl Sync for ffi::TopoDS_Shape {}
 
 unsafe impl Send for ffi::TopExp_Explorer {}
 unsafe impl Send for ffi::BRepFilletAPI_MakeChamfer {}
