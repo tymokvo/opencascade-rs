c31db5934c72c22d4c4b205c65b4940299b0be5a
Tyler Kvochick <tyler.kvochick@teecom.com>
2026-03-24 14:15:24 -0400

    Add axes and cubic shape

--- examples/src/project_shape.rs
+++ examples/src/project_shape.rs
@@ -1,5 +1,24 @@
-use opencascade::primitives::Shape;
+use opencascade::{
+    angle::Angle,
+    primitives::{Compound, IntoShape, Shape},
+    workplane::Workplane,
+};
 
 pub fn shape() -> Shape {
-    Shape::sphere(16.0).build()
+    let axes = Workplane::xy()
+        .sketch()
+        .line_to(1.0, 0.0)
+        .move_to(0.0, 0.0)
+        .line_to(0.0, 1.0)
+        .wire()
+        .into_shape();
+    let wp = Workplane::xy()
+        .rotated(opencascade::angle::RVec {
+            x: Angle::Degrees(45.0),
+            y: Angle::Degrees(45.0),
+            z: Angle::Degrees(0.0),
+        })
+        .translated(glam::dvec3(0.0, 0.0, 16.0));
+    let extrusion = wp.rect(4.0, 8.0).to_face().extrude(wp.normal()).into_shape();
+    Compound::from_shapes([axes, extrusion]).into_shape()
 }
