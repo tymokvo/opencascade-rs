f261595d95e1ed62f50bfc69110111ad9c90ffb4
Tyler Kvochick <tyler.kvochick@teecom.com>
2026-08-24 23:46:22 -0400

    Include and link in opencascade-sys

--- crates/opencascade-sys/build.rs
+++ crates/opencascade-sys/build.rs
@@ -22,11 +22,45 @@ fn main() {
         build.include(current.parent().unwrap());
     }
 
+    // HACK: Ultra-hack. This re-emits the link lines for cxx to link properly.
+    // Can pass through cargo metadata?
+    const OCCT_LIBS: &[&str] = &[
+        "TKMath",
+        "TKernel",
+        "TKDE",
+        "TKFeat",
+        "TKGeomBase",
+        "TKG2d",
+        "TKG3d",
+        "TKTopAlgo",
+        "TKGeomAlgo",
+        "TKBRep",
+        "TKPrim",
+        "TKDESTEP",
+        "TKDEIGES",
+        "TKDESTL",
+        "TKMesh",
+        "TKHLR",
+        "TKShHealing",
+        "TKFillet",
+        "TKBool",
+        "TKBO",
+        "TKOffset",
+        "TKXSBase",
+        "TKCAF",
+        "TKLCAF",
+        "TKXCAF",
+    ];
+
+    for lib in OCCT_LIBS {
+        println!("cargo::rustc-link-lib=static={}", lib);
+    }
+
     build
         .cpp(true)
         .flag_if_supported("-std=c++11")
         .define("_USE_MATH_DEFINES", "TRUE")
-        .include(std::env::var("todo: read DEP_OCCT_INCLUDE from cargo metadata").unwrap())
+        .include(std::env::var("DEP_OCCT_INCLUDE").unwrap())
         .include("include")
         .compile("wrapper");
 
